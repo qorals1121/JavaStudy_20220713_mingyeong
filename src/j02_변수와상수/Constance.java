02fcb3cdf1de4d2849ea76fd4d7d9b6f891d1d31
qorals1121 <qorals1121@naver.com>
2022-07-15 11:06:13 +0900

    챕터2 변수와 상수

---
+++ src/j02_변수와상수/Constance.java
@@ -0,0 +1,15 @@
+package j02_변수와상수;
+
+public class Constance {
+	public static void main(String[] args) {
+		final int MAX_NUMBER = 100;
+		final int MIN_NUMBER = 10;
+		int number = 50;
+		
+		System.out.println("최소값 : " + MIN_NUMBER);
+		System.out.println("최대값 : " + MAX_NUMBER);
+		System.out.println("현재값 : " + number);
+		
+	}
+
+}
