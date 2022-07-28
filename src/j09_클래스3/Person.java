f58a011f33a76774d1d28d60bf46a217eeffbc98
qorals1121 <qorals1121@naver.com>
2022-07-28 12:51:50 +0900

    챕터 9 클래스3

---
+++ src/j09_클래스3/Person.java
@@ -0,0 +1,27 @@
+package j09_클래스3;
+
+public class Person {
+	String name;
+	int age;
+	
+	public Person() {
+		
+	}
+	
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+
+}
