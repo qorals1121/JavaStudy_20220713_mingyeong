e6016615c2190dbf76fdc0f4e90189aefd8fb76d
qorals1121 <qorals1121@naver.com>
2022-08-02 12:34:15 +0900

    챕터14 lombok

--- src/j14_lombok/Car.java
+++ src/j14_lombok/Car.java
@@ -1,6 +1,7 @@
 package j14_lombok;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Car {
 	private String company;
 	private String model;
